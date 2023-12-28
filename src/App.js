f422393d3241d64a58735099bdb21c27b2c77e83
muhammedanaskhan <muhammedanas5186@gmail.com>
2023-12-27 21:53:46 -0800

    about VR section added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Banner from './Banner/Banner';
+import IntroSection from './IntroSection/IntroSection';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
+      <IntroSection/>
     </div>
   );
 }
---
+++ src/IntroSection/IntroSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './IntroSection.css'
+
+function IntroSection() {
+    return (
+        <div className='intro-section'>
+            <div className='content-box'>
+                <img src="/images/VR_Image.png" alt="" />
+                <div className='txt-box'>
+                    <p className='heading'>The leader in interactive VR</p>
+                    <p className='about'>Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.</p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default IntroSection
